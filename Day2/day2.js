67f455f64c0f5e530435105d4ab7d71be7b75f5b
shashwat <mishrashashwat112@gmail.com>
2025-06-14 19:47:02 +0530

    Created a simple simple calculator to perform addition

---
+++ Day2/day2.js
@@ -0,0 +1,6 @@
+function calculate(){
+    const input1= document.getElementById("input1").value;
+    const input2= document.getElementById("input2").value; 
+    const result=Number(input1)+Number(input2);   
+    console.log(result);
+}
