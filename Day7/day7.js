ad975046bea07f760703f232e28620f33ebb8c37
shashwat <mishrashashwat112@gmail.com>
2025-07-06 20:00:47 +0530

    used map function to copy the element of array into different array

--- Day7/day7.js
+++ Day7/day7.js
@@ -1,4 +1,7 @@
 let nums=[20, 30, 40, 50, 60, 70, 80, 90, 100];
-nums.map((val)=>{
-    console.log(val);
+// nums.map((val)=>{
+//     console.log(val);
+// })
+ let newArray=nums.map((val)=>{
+    return val;
 })
