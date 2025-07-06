dc37306d4178a5b2fa716a9c645930491b2ea4a4
shashwat <mishrashashwat112@gmail.com>
2025-07-06 20:08:05 +0530

    usnig filter method to get array of even numberd

--- Day7/day7.js
+++ Day7/day7.js
@@ -1,7 +1,14 @@
-let nums=[20, 30, 40, 50, 60, 70, 80, 90, 100];
+let nums=[20, 30, 33, 55, 5, 70, 85, 90, 100];
 // nums.map((val)=>{
 //     console.log(val);
 // })
- let newArray=nums.map((val)=>{
-    return val;
+//  let newArray=nums.map((val)=>{
+//     return val;
+// })
+
+// usnig filter to filter out even numbers
+let evenNums=nums.filter((val)=>
+{
+    return val%2===0;
 })
+console.log(evenNums);
